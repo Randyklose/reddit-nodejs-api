5bac6ea67577e4ce11f8a49307812d5bbd758a48
Randy Klose <randyklose@gmail.com>
2017-01-26 21:49:02 +0000

    getAllPosts function

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
 // load the mysql library
 var mysql = require('promise-mysql');
 
+
 // create a connection to our Cloud9 server
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
   host     : 'localhost',
   user     : 'randyk', // CHANGE THIS :)
   password : '',
@@ -14,25 +15,59 @@ var reddit = require('./reddit');
 var redditAPI = reddit(connection);
 
 // It's request time!
-redditAPI.createUser({
-  username: 'hello23',
-  password: 'xxx'
-}, function(err, user) {
-  if (err) {
-    console.log(err);
+
+
+// return redditAPI.createUser({
+//   username: 'hello23',
+//   password: 'xxx'
+// })
+// .then(function(user) {
+//       redditAPI.createPost({
+//       title: 'hi reddit!',
+//       url: 'https://www.reddit.com',
+//       userId: user.id
+// })
+// })
+
+// .then(function(post) {
+//   console.log(post);
+// })
+// .catch(function(err) {
+//     console.log("There was an: ",err)
+// })
+
+redditAPI.getAllPosts({})
+.then(function(result){
+  
+  var posts= [];
+  result.forEach(function(row){
+    var post = posts.find(function(post){
+           return post.userId === row.userId;
+    })
+  
+  if(!post) {
+    post = {
+      id:row.postId,
+      title:row.postTitle,
+      url:row.url,
+      createdAt:row.postCreatedAt,
+      UpdatedAt:row.postUpdatedAt,
+      user:[]
+    }
+
+      posts.push(post)
   }
-  else {
-    redditAPI.createPost({
-      title: 'hi reddit!',
-      url: 'https://www.reddit.com',
-      userId: user.id
-    }, function(err, post) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log(post);
-      }
-    });
-  }
-});
+      post.user.push({
+      id:row.userId,
+      username:row.username,
+      createdAt:row.userCreatedAt,
+      updatedAt:row.userUpdatedAt
+    })
+  
+})
+return posts;
+})
+.then(function(result){
+  console.log(JSON.stringify(result,null,4));
+  connection.end()
+  })
